f7ce85986c9fd655a78717be3afaf8f13bd906c3
LiXu <hugh@contek.io>
2025-10-14 15:33:35 +0800

    feat: 增加目录创建测试

--- test_cephfs_io.py
+++ test_cephfs_io.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 
 node = sys.argv[1]
@@ -9,6 +10,7 @@ print(f"{node=}, {working_dir=}")
 output_dir = os.path.join(working_dir, "output")
 os.makedirs(output_dir, exist_ok=True)
 
+# 文件读写
 output_file = os.path.join(output_dir, node)
 if os.path.isfile(output_file):
     os.remove(output_file)
@@ -21,6 +23,14 @@ with open(output_file, "r") as f:
 
 assert node == raw
 
+# 目录创建
+dir_test = os.path.join(output_dir, node+"_test_dir")
+if os.path.isdir(dir_test):
+    shutil.rmtree(dir_test)
+os.makedirs(dir_test)
+assert os.path.isdir(dir_test)
+
+# max pid 配置
 with open("/sys/fs/cgroup/pids.max", "r") as f:
     pid_max_config = f.read().strip()
 
